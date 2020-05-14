067500abd22fc90393ae3bd7a67bd70b5a006226
Lucas M Cardoso <clucasmedeiros@gmail.com>
2020-05-14 20:53:32 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
-import { MissingParamError, InvalidParamError } from '../errors'
+import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
@@ -7,21 +7,28 @@ import { EmailValidator } from '../protocols/email-validator'
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator
 
-    constructor (emailValidator: EmailValidator) {
+    constructor(emailValidator: EmailValidator) {
         this.emailValidator = emailValidator
     }
 
-    handle (httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-        for (const field of requiredFields) {
-            if (!httpRequest.body[field]) {
-                return badRequest(new MissingParamError(`${field}`))
+    handle(httpRequest: HttpRequest): HttpResponse {
+        try {
+            const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    return badRequest(new MissingParamError(`${field}`))
+                }
             }
-        }
 
-        const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
-        if (!isValidEmail) {
-            return badRequest(new InvalidParamError('email'))
+            const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
+            if (!isValidEmail) {
+                return badRequest(new InvalidParamError('email'))
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: new ServerError()
+            }
         }
     }
 }
