eeb9fb139228488bc54c81ee053df358143fbb0a
Lucas M Cardoso <clucasmedeiros@gmail.com>
2020-05-20 20:56:14 -0300

    chore: ignore main layer on jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
     collectCoverageFrom: [
         '<rootDir>/src/**/*.ts',
         '!<rootDir>/src/**/index.ts',
-        '!<rootDir>/src/**/*-protocols.ts'
+        '!<rootDir>/src/**/*-protocols.ts',
+        '!<rootDir>/src/main/**'
     ],
     coverageDirectory: 'coverage',
     preset: '@shelf/jest-mongodb',
